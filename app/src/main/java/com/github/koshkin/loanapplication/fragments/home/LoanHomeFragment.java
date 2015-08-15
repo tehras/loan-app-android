f74988837dccec57f2836742f4b61301e1edbc6a
tehras <tkoshkin@gmail.com>
2015-08-14 21:01:14 -0400

    cleanup

--- app/src/main/java/com/github/koshkin/loanapplication/fragments/home/LoanHomeFragment.java
+++ app/src/main/java/com/github/koshkin/loanapplication/fragments/home/LoanHomeFragment.java
@@ -10,9 +10,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.github.clans.fab.FloatingActionButton;
@@ -51,8 +52,6 @@ public class LoanHomeFragment extends BaseFragment implements AsyncTaskCallbackI
     private ObservableRecyclerView mRecyclerView;
     private FrameLayout mLineChartViewHolder;
     private LoanCacheObject mLoanCacheObject;
-    private ScrollView mScrollView;
-    private View.OnClickListener mAddNewLoanButtonListener;
     private FloatingActionButton mAddNewLoan;
     private TextView mCurrentAmount, mPreviousAmount, mPreviousAmountLabel;
     private RelativeLayout mHeaderContainer;
@@ -81,6 +80,7 @@ public class LoanHomeFragment extends BaseFragment implements AsyncTaskCallbackI
 
         mLineChartView = (LineChart) rootView.findViewById(R.id.home_page_chart_view);
         mLineChartViewHolder = (FrameLayout) rootView.findViewById(R.id.card_view);
+
 
         //TODO remove this later
         updateGraphView(null);
@@ -277,10 +277,13 @@ public class LoanHomeFragment extends BaseFragment implements AsyncTaskCallbackI
         //Logic for the FAB button
         int difference = correctedScrollY - mPreviousScrollY;
 
-        if (Math.abs(correctedScrollY) > 100)
+        if (difference > 0) {
             mAddNewLoan.setTranslationY(mPreviousScrollY);
-        else
+        } else {
+            Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.abc_slide_in_bottom);
             mAddNewLoan.setTranslationY(0);
+            mAddNewLoan.startAnimation(animation);
+        }
 
         mPreviousScrollY = correctedScrollY;
     }
